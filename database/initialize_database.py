ec9de10e20e1439b9e7edbf0b431a050394d8872
14ksmith <ksmith2017@gmail.com>
2022-07-15 12:50:15 -0400

    Changed name of database to period_tracker.db instead of period_tracking_app.db.

--- database/initialize_database.py
+++ database/initialize_database.py
@@ -3,12 +3,12 @@ import sqlite3
 
 
 def initialize_engine():
-    engine = create_engine("sqlite:///period_tracking_app.db")
+    engine = create_engine("sqlite:///period_tracker.db")
     return engine
 
 
 def get_db_connection():
     """Get a connection to the database that can be used with the Flask app"""
-    conn = sqlite3.connect("period_tracking_app.db")
+    conn = sqlite3.connect("period_tracker.db")
     conn.row_factory = sqlite3.Row
     return conn
